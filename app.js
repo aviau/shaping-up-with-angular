b40074fb8419fcbc51dabb25ca5197c693fc92c1
aviau <alexandre.viau@savoirfairelinux.com>
2014-10-06 23:09:40 -0400

    Moved some stuff in product.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-products']);
 
   app.controller('StoreController', function() {
     this.products = gems;
@@ -23,73 +23,6 @@
       this.review = {};
     };
   });
-
-  app.directive("productSpecs", function() {
-    return {
-      restrict:"A",
-      templateUrl: "product-specs.html"
-    };
-  });
-
-  app.directive("productDescription", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-description.html"
-    };
-  });
-
-app.directive('productGallery', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'product-gallery.html',
-    controller: function() {
-      this.current = 0;
-      this.setCurrent = function(imageNumber) {
-        this.current = imageNumber || 0;
-      };
-    },
-    controllerAs: 'gallery'
-  };
-});
-
- app.directive('productGallery', function() {
-  return {
-    restrict: 'E'
-  };
-});
-
-  app.directive('productTitle', function(){
-      return{
-        restrict: 'E',
-        templateUrl: 'product-title.html'
-      };
-  });
-
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
-
-  app.directive('productTabs', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'product-tabs.html',
-    controller: function() {
-      this.tab = 1;
-
-      this.isSet = function(checkTab) {
-        return this.tab === checkTab;
-      };
-
-      this.setTab = function(activeTab) {
-        this.tab = activeTab;
-      };
-    },
-    controllerAs: 'tab'
-  };
- });
 
   var gems = [
     {
